4ec00913a44c5a3843147e864beff246f38bc19f
ShriramShagi <shagrisrm@gmail.com>
2023-06-16 10:24:09 +0530

    Initial Commit

--- src/internal/initialize.js
+++ src/internal/initialize.js
@@ -1,8 +1,6 @@
 const { BotBuilderCloudAdapter } = require("@microsoft/teamsfx");
 const ConversationBot = BotBuilderCloudAdapter.ConversationBot;
-const { DoStuffActionHandler } = require("../cardActions/doStuffActionHandler");
 const { SubmitActionHandler } = require("../cardActions/submitActionHandler");
-const { HelloWorldCommandHandler } = require("../commands/helloworldCommandHandler");
 const config = require("./config");
 const { CreateIncidentCommandHandler } = require("../commands/createIncidentCommandHandler");
 const { ConfirmActionHandler } = require("../cardActions/confirmActionHandler");
@@ -18,11 +16,11 @@ const workflowApp = new ConversationBot({
   },
   command: {
     enabled: true,
-    commands: [new HelloWorldCommandHandler(), new CreateIncidentCommandHandler()],
+    commands: [new CreateIncidentCommandHandler()],
   },
   cardAction: {
     enabled: true,
-    actions: [new DoStuffActionHandler(), new SubmitActionHandler(), new ConfirmActionHandler()],
+    actions: [new SubmitActionHandler(), new ConfirmActionHandler()],
   },
 });
 
